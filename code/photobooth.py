aef5d35f65fb3f8509c818858c2b16ded5629cfd
Ben Nuttall <ben@bennuttall.com>
2015-05-23 16:11:55 +0100

    Tidy up code

--- code/photobooth.py
+++ code/photobooth.py
@@ -1,33 +1,27 @@
-from mcpi import minecraft​
-import time
-import picamera
+from mcpi import minecraft
+from picamera import PiCamera
+from time import sleep
+
+def take_the_pic():
+    with PiCamera() as camera:
+        camera.start_preview()
+        sleep(2)
+        camera.capture('/home/pi/selfie.jpg')
 
 mc = minecraft.Minecraft.create()
 
-def take_the_pic():
-    with picamera.PiCamera() as camera:
-        camera.start_preview()
-        time.sleep(2)
-        camera.capture('selfie.jpg')
+mc.postToChat("Find the Photobooth")
 
-def where_am_i():
-    while True:
-        x, y, z = mc.player.getPos()
-           
-        time.sleep(3)
-        
-        if x >= 10.5 and y == 9.0 and z == -44.3:
-        #print "You are at the photobooth!"
-        
-            mc.postToChat("You are in the Photobooth!")
-            time.sleep(1)
-            mc.postToChat("Smile!")
-            time.sleep(1)
-            take_the_pic()
-            mc.postToChat("Check out your picture")
-            time.sleep(5)
-        
-mc.postToChat("Find the Photo-Booth")
-where_am_I()
+while True:
+    x, y, z = mc.player.getPos()
+       
+    sleep(3)
     
-
+    if x >= 10.5 and y == 9.0 and z == -44.3:
+        mc.postToChat("You are in the Photobooth!")
+        sleep(1)
+        mc.postToChat("Smile!")
+        sleep(1)
+        take_the_pic()
+        mc.postToChat("Check out your picture")
+        sleep(5)
